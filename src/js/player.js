ea02de2e78a27dd09678f075432045cfcd92fc62
seanat0r <seanatorhd@gmail.com>
2025-08-05 17:11:48 +0200

    added in player class the constructor to create player

--- src/js/player.js
+++ src/js/player.js
@@ -1,3 +1,10 @@
-class Player {
-	constructor() {}
+import Gameboard from './gameboard'
+export default class Player {
+  constructor (name) {
+	this.name = name;
+
+    this.ownBoard = this.#gameboard.createBoard()
+  }
+
+  #gameboard = new Gameboard()
 }
