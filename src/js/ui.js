2f76638632023edf1a712cd3523e11fad0b4fa57
seanat0r <seanatorhd@gmail.com>
2025-08-06 10:31:14 +0200

    Initialize base UI structure in ui.js

---
+++ src/js/ui.js
@@ -0,0 +1,11 @@
+//* Use for Global DOM Structure
+export class AppUI {}
+
+//* Use for placing the ships with UI
+export class ShipPlacementUI {}
+
+//* Change the UI accordingly for the battle against the AI
+export class AIGameUI {}
+
+//* Change the UI accordingly for the battle against a local player (2 real users on the same PC)
+export class LocalGameUI {}
